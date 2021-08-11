6536f3b2f3b35f0220f5a36acfc786702283a151
Jethro Olwen <=>
2021-08-11 13:02:03 +0700

    deploy x.x.x

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/test-site/'
+    ? '/afurerucolors@github.io/'
     : '/',
   transpileDependencies: [
     'vuetify'
