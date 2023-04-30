ef42a2ebd4deaafa86608a825a4ae567cb6782dc
Jethro Olwen <=>
2023-04-30 14:31:37 +0700

    update meta information

--- vue.config.js
+++ vue.config.js
@@ -9,9 +9,9 @@ module.exports = {
             .tap(args => {
                 args[0].title = 'Afureru Colors';
                 args[0].meta = {
-                    'description': 'Digital Illustrator. I do anime, game, movie fan arts and original characters.',
-                    'keywords': 'Anime, Fan Art, Commission, Artist, Original Character',
-                    'author': 'Afureru Colors',
+                    'description': 'Artist/Illustrator. I draw fanarts, original character and concept art.',
+                    'keywords': 'illustration, illust, fan art, commission, afureru colors',
+                    'author': '(溢れる) Afureru Colors',
                     'theme-color': '#E0F7FA',
                 };
 
